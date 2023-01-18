5cd86bd6b550f7b09edfc1807b0c186591feea87
Kyle Reynolds <kbreyn06@gmail.com>
2023-01-17 23:51:23 -0600

    added icons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import logo from './dudelogo.jpg';
+import venmo from './venmoicon.png';
+import facebook from './facebookicon.png';
+import instagram from './instaicon.png';
 import './App.css';
 
-import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import PaidIcon from '@mui/icons-material/Paid';
+
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className="links" href="https://account.venmo.com/u/nolan_smith" ><PaidIcon />VENMO</a>
-        <a className="links" href="https://www.facebook.com/profile.php?id=100084473899055"><FacebookOutlinedIcon />FACEBOOK</a>
-        <a className="links" href="https://www.instagram.com/cowabungadudekc/"><InstagramIcon />INSTAGRAM</a>
+      <img src={logo} className="App-logo" alt="logo" />
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+          <a className="links" href="https://account.venmo.com/u/nolan_smith" >
+            <img
+          className="venmo"
+          alt="venmo"
+          src={venmo}
+        />
+        </a>
+          </Grid>  
+          <Grid item xs={6}>
+          <a className="links" href="https://www.facebook.com/profile.php?id=100084473899055"><img
+          className="social"
+          alt="facebook"
+          src={facebook}
+        /></a>
+          </Grid>
+          <Grid item xs={6}>
+          <a className="links" href="https://www.instagram.com/cowabungadudekc/"><img
+          className="social"
+          alt="instagram"
+          src={instagram}
+        /></a>
+          </Grid>
+        </Grid>
       </header>
     </div>
   );
