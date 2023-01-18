4476abb5a250d9b3943a0d5265908f6563e47176
Kyle Reynolds <kbreyn06@gmail.com>
2023-01-18 00:39:04 -0600

    added info and corrected spacing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './dudelogo.jpg';
+import logo from './dudelogo.png';
 import venmo from './venmoicon.png';
 import facebook from './facebookicon.png';
 import instagram from './instaicon.png';
@@ -6,12 +6,21 @@ import './App.css';
 
 
 import Grid from '@mui/material/Grid';
-
+import Divider from '@mui/material/Divider';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div className='info'>
+      <Divider>UPCOMING SHOWS</Divider>
+      <Divider>1/14/23 - Jerry's Baitshop</Divider>
+      <Divider>TBD</Divider>
+      <Divider>TBD</Divider>
+      </div>
+      
+      <body className='App-body'>
         <Grid container spacing={2}>
           <Grid item xs={12}>
           <a className="links" href="https://account.venmo.com/u/nolan_smith" >
@@ -37,7 +46,8 @@ function App() {
         /></a>
           </Grid>
         </Grid>
-      </header>
+        </body>
+      
     </div>
   );
 }
